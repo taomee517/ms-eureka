642bc1d6606e69d1b6737936fe0bbbcc87b32878
taomee517 <taomee517@qq.com>
2019-12-29 10:09:51 +0800

    Initial commit

---
+++ src/main/java/com/demo/ms/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.demo.ms.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka server
+ * @author tuliangcheng
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
